bece23fbc07d34c189fbe8539d6bc92123183be8
kohei-isoda <koheiisoda@KoheiIsodanoMacBook-Air.local>
2017-12-15 20:37:06 +0900

    Add edit group function

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
   resource :user, only: [:edit, :update]
-  resource :group, only: [:new, :create]
+  resource :group, only: [:new, :create, :edit, :update]
 end
