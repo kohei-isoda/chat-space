dab31558590204ce894699996fb3670faecde4c0
kohei-isoda <koheiisoda@KoheiIsodanoMacBook-Air.local>
2017-12-18 16:45:39 +0900

    Fix view and routing
    ・そのユーザーを編集したいかわかるように引数current_userを追加
    ・ルーティングを命名規則に従って修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resources :user, only: [:edit, :update]
-  resources :group, only: [:new, :create, :edit, :update]
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update]
 end
