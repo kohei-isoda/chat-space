df8895859a6e3d754ac089f19acfa2c14bb13ed6
kohei-isoda <koheiisoda@KoheiIsodanoMacBook-Air.local>
2017-12-24 16:28:50 +0900

    Add message send function

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
 
-  # has_many :messages
+  has_many :messages
   has_many :members
   has_many :groups, through: :members
 end
