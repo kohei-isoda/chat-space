2b4d630184fd467e12c452f18d8979407b6c25ca
kohei-isoda <koheiisoda@KoheiIsodanoMacBook-Air.local>
2018-01-03 17:29:57 +0900

    Modify model

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,7 +3,7 @@ class Group < ApplicationRecord
   has_many :messages
   has_many :users, through: :members
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
 
   def show_last_message
     messages.last.try(:body) || "まだメッセージはありません。"
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,7 +3,5 @@ class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
 
-  validates :name, presence: true
-
   mount_uploader :image, ImageUploader
 end
