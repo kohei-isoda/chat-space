7c3f11fe69131219c2ab58f1ad5fa244a2fde4b5
kohei-isoda <koheiisoda@KoheiIsodanoMacBook-Air.local>
2018-01-08 15:06:53 +0900

    Add json jbuilder

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,27 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(10)
+    respond_to do |format|
+      format.json
+    end
+  end
+
   def edit
   end
 
   def update
+    flash.now[:notice] = ""
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to :root, notice: "SUCCESS!"
+    else
+      render :edit, notice: "failed!"
+    end
   end
 
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
