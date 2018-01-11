0203db120479b11589870095904203ab4477c8bf
kohei-isoda <koheiisoda@KoheiIsodanoMacBook-Air.local>
2018-01-11 18:33:41 +0900

    Edit controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(10)
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
     respond_to do |format|
       format.json
+      format.html
     end
   end
 
